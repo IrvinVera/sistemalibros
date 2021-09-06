6d9b8d5001e1990c577d119457f7e5569403d525
IrvinVera <IrvinVera@users.noreply.github.com>
2021-09-06 11:25:19 -0500

    Se quito middleware para probar métodos

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -8,11 +8,6 @@ use Illuminate\Http\Request;
 class CategoriaController extends Controller
 {
 
-    public function __construct()
-    {
-         $this->middleware('auth');
-    }
-    
     public function listarCategorias(){
 
         $listaCategorias = Categoria::all();
